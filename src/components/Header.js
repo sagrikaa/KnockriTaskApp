f04db32cf2ae388e3d083e29e3b362014b6d03d6
sagrikaa <40899302+sagrikaa@users.noreply.github.com>
2019-06-22 14:05:03 -0400

    Front-end done

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <span className='header-text'>Knockri Applicants</span>
+        </div>
+    )
+}
